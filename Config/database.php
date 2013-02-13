beab0ac3364b258a4a20a67b4a9e86e63e13f56b
ktymttsuy <Ka0d5C0pe>
2013-02-13 23:33:36 +0900

    bakeによる初期バージョン

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'things_user',
+		'password' => 'things_pass',
+		'database' => 'Things',
+	);
+}
